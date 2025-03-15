c572a87ed58fb01bf0ce6a38507e63c6bee689fc
advin <adithpsycho@gmail.com>
2025-03-15 13:48:09 +0530

    [NEW] added login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm!:FormGroup
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.invalid) return
+
+    console.log(this.loginForm.value);
+    
+  }
+
+  
 }
