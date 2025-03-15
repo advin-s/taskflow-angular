be39d73ccee95e7e86deefeb78eb5a0cc841541e
advin <adithpsycho@gmail.com>
2025-03-15 13:17:08 +0530

    [FEAT] auth page ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:AuthComponent,
+        children:[
+            {
+                path:'login',
+                component:LoginComponent
+            },
+            {
+                path:'signup',
+                component:SignupComponent
+            }
+        ]
+    }
+];
