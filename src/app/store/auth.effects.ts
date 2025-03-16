fde69a1459d3e4f88bd6d3e28001f3bbb4f5756b
advin <adithpsycho@gmail.com>
2025-03-16 20:20:58 +0530

    [FEAT] added routing after auth success

--- src/app/store/auth.effects.ts
+++ src/app/store/auth.effects.ts
@@ -1,4 +1,4 @@
-import { Inject, inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import {
   login,
@@ -10,11 +10,13 @@ import {
 } from './auth.actions';
 import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthEffects {
   private actions$ = inject(Actions);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   login$ = createEffect(() =>
     this.actions$.pipe(
@@ -22,6 +24,7 @@ export class AuthEffects {
       switchMap((action) =>
         this.authService.onLogin({ ...action }).pipe(
           tap((res) => {
+            this.router.navigate(['/tasks']);
             localStorage.setItem('token', res.token);
           }),
           map((loginRes) => loginSuccess({ auth: loginRes })),
@@ -35,17 +38,15 @@ export class AuthEffects {
     this.actions$.pipe(
       ofType(signup),
       switchMap(({ signup }) =>
-        this.authService
-          .onSignup(signup)
-          .pipe(
-            tap((res) => localStorage.setItem('token', res.token)),
-            map((res) => signupSuccess({auth:res})),
-            catchError(error => of(signupFailed({error})))
-          )
-
+        this.authService.onSignup(signup).pipe(
+          tap((res) => {
+            this.router.navigate(['/tasks']);
+            localStorage.setItem('token', res.token);
+          }),
+          map((res) => signupSuccess({ auth: res })),
+          catchError((error) => of(signupFailed({ error })))
+        )
       )
     )
   );
-
 }
-
