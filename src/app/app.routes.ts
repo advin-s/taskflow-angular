c4791bc79ce3646584a81d23c2bab0780afe6a95
advin <adithpsycho@gmail.com>
2025-03-16 20:19:53 +0530

    [FEAT] added tasks route and canMathc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,56 @@
-import { Routes } from '@angular/router';
+import { CanActivateFn, CanMatchFn, Router, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+import { selectAuth } from './store/auth.selector';
+
+export const authGuard: CanMatchFn = () => {
+  const store$ = inject(Store);
+  const router = inject(Router);
+
+  return store$
+    .select(selectAuth)
+    .pipe(
+      map((auth) => {
+        console.log(auth);
+        
+        if (auth && auth.token) {
+          return true;
+        } else {
+          router.navigate(['']);
+          return false;
+        }
+      })
+    );
+};
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:AuthComponent,
-        children:[
-            {
-                path:'',
-                redirectTo:'login',
-                pathMatch:'full'
-            },
-            {   
-                path:'login',
-                component:LoginComponent
-            },
-            {
-                path:'signup',
-                component:SignupComponent
-            }
-        ]
-    }
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+    ],
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./tasks/tasks.component').then((c) => c.TasksComponent),
+    canMatch: [authGuard],
+  },
 ];
